7e187cee8775480873f888e79f1cfb9ef3f80908
Tomohiro Kumagai <tomohiro@ez-style.jp>
2013-01-14 14:49:11 +0900

    呼び出し順番が判りやすいように、カウンタの更新に遅延を設定しました。

--- EzSample_iOS_UIViewTouch/EzSampleViewController.m
+++ EzSample_iOS_UIViewTouch/EzSampleViewController.m
@@ -10,13 +10,24 @@
 
 @interface EzSampleViewController ()
 
+// 実行順序が判るように、時間を遅らせてカウンタを描画します。
+- (void)EzSetCountToLabel:(NSArray*)countAndTargetLabel;
+
+@property (atomic,readwrite) BOOL EzWriting;
+
 @end
 
 @implementation EzSampleViewController
+{
+	BOOL _writing;
+	NSLock* _writingLock;
+}
 
 - (void)viewDidLoad
 {
 	[super viewDidLoad];
+	
+	_writingLock = [[NSLock alloc] init];
 	
 	[self baseViewUserInteractionEnabled:self.baseViewUserInteractionSwitch];
 }
@@ -50,7 +61,7 @@
 	{
 		targetLabel = (began ? self.countLabelForNormalBackgroundViewBegan : self.countLabelForNormalBackgroundViewEnded);
 	}
-	else if ([viewName isEqualToString:@"Highlighed Background View"])
+	else if ([viewName isEqualToString:@"Highlighted Background View"])
 	{
 		targetLabel = (began ? self.countLabelForHighlightedBackgroundViewBegan : self.countLabelForHighlightedBackgroundViewEnded);
 	}
@@ -59,7 +70,66 @@
 		targetLabel = nil;
 	}
 	
-	targetLabel.text = [[NSString alloc] initWithFormat:@"%d", targetLabel.text.integerValue + 1];
+	if (targetLabel)
+	{
+		NSNumber* value = [[NSNumber alloc] initWithInteger:targetLabel.text.integerValue + 1];
+
+		// 書き込み順番が判るように遅らせて出力します。そのためにバックグラウンドにいったん制御を渡します。
+		[self performSelectorInBackground:@selector(EzSetCountToLabel:) withObject:@[ value, targetLabel ]];
+	}
+}
+
+- (void)setEzWriting:(BOOL)EzWriting
+{
+	[_writingLock lock];
+	
+	_writing = EzWriting;
+	
+	[_writingLock unlock];
+}
+
+- (BOOL)EzWriting
+{
+	[_writingLock lock];
+	
+	@try
+	{
+		return _writing;
+	}
+	@finally
+	{
+		[_writingLock unlock];
+	}
+}
+
+- (void)EzSetCountToLabel:(NSArray *)countAndTargetLabel
+{
+	// 誰かが書き込み中の時は待ちます。
+	while (self.EzWriting)
+	{
+		[NSThread sleepForTimeInterval:0.01];
+	}
+
+	// だれも書き込まない状態になったらカウンタを更新します。
+	[_writingLock lock];
+	
+	@try
+	{
+		_writing = YES;
+
+		NSNumber* count = countAndTargetLabel[0];
+		UILabel* label = countAndTargetLabel[1];
+			
+		[label performSelectorOnMainThread:@selector(setText:) withObject:count.description waitUntilDone:NO];
+			
+		[NSThread sleepForTimeInterval:0.24];
+	}
+	@finally
+	{
+		_writing = NO;
+		
+		[_writingLock unlock];
+	}
 }
 
 @end
